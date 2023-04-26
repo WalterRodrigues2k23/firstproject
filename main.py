779de2b635f5985b11305ad33d07ed7e3c4502b2
Walter Rodrigues dos Santos Junior <metal.walter@gmail.com>
2023-04-25 22:34:53 -0300

    Criando o cronometro

---
+++ main.py
@@ -0,0 +1,28 @@
+import time
+
+starttime = time.time()
+lasttime = starttime
+lapnum = 1
+value = ""
+  
+print("Aperte ENTER Para gravar o seu tempo.\n Aperte Q e aperte ENTER para finalizar o seu treino.")
+  
+while value.lower() != "q":
+              
+    # Input for the ENTER key press
+    value = input()
+  
+    # The current lap-time
+    laptime = round((time.time() - lasttime), 2)
+  
+    # Printing the lap number, lap-time, and total time
+    print("Tempo No. "+str(lapnum))
+    print("Seu tempo foi: "+str(laptime), "segundos.")
+            
+    print("_"*20)
+  
+    # Updating the previous total time and lap number
+    lasttime = time.time()
+    lapnum += 1
+  
+print("Treinamento concluído!")
