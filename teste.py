0e4a3c17bf9288dbb62d27bff34f03247167613b
Walter Rodrigues dos Santos Junior <metal.walter@gmail.com>
2023-04-29 08:02:10 -0300

    Versão quase final

--- teste.py
+++ teste.py
@@ -1,12 +1,18 @@
 import time
 
-time.sleep(1)
-while True:
-  input("Enter")
-  tempoini = time.time()
-  while True:
-    input("")
-    tempofim = time.time()
+def print_time(total_seconds):
+    total_mins = total_seconds / 60
+    seconds = int(total_seconds % 60)
+    hours = int(total_mins / 60)
+    mins = int(total_mins % 60)
+    print('Tempo total: {} horas: {} minutos: {} segundos.'.format(hours(00), mins(00), seconds(00),))
 
-    time.perf_counter_ns = tempofim - tempoini
-    print(time.perf_counter_ns)
+input('Aperte ENTER para começar: ')
+start_time = time.time()
+
+print('Cronometro ativo')
+
+input('Aperte ENTER para parar: ')
+stop_time = time.time()
+
+print_time(stop_time - start_time)
