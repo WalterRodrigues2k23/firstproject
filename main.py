8a408370c0ab65ddf1b388252e31ff3837689717
Walter Rodrigues dos Santos Junior <metal.walter@gmail.com>
2023-04-28 22:06:09 -0300

    Mais um timer

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
 import time
 
-starttime = time.time()
-lasttime = starttime
-lapnum = 1
-value = ""
-  
-print("Aperte ENTER Para gravar o seu tempo.\n Aperte Q e aperte ENTER para finalizar o seu treino.")
-  
-while value.lower() != "q":
-    # Input for the ENTER key press
-    value = input()
-  
-    # The current lap-time
-    laptime = round((time.time() - lasttime), 2)
-  
-    # Printing the lap number, lap-time, and total time
-    print("Tempo No. "+str(lapnum))
-    print("Seu tempo foi: "+str(laptime), "segundos.")
-            
-    print("-"*20)
-  
-    # Updating the previous total time and lap number
-    lasttime = time.time()
-    lapnum += 1
-  
-print("Treinamento concluído!")
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range(my_time, 0, +1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("TIME'S UP!")
